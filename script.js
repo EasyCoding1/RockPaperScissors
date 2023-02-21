c765f17b461bd2f57b4a8961a35f60e848ad8a22
easycoding1 <tsjr96@gmail.com>
2023-02-21 15:02:29 +0000

    Added 'playround' function to compare answer of player vs pc and determine a winner, with a message to indicate win, loss, draw. Now will look at score then looping whole game

--- script.js
+++ script.js
@@ -17,23 +17,66 @@ function getComputerChoice() {
 }
 
 const computerChoice = getComputerChoice();
-console.log("Computer choice: " + computerChoice);
 
  
 
 
 
-
+const po1 = "Rock";
+const po2 = "Paper";
+const po3 = "Scissors";
 function getPlayerChoice () {
     let answer = prompt("Select your choice. Enter: 'Rock' , 'Paper', or 'Scissors' ");
     while (answer != "Rock" &&  answer !== "Paper" && answer !== "Scissors") {
       answer = prompt("Sorry, please enter exactly 'Rock' , 'Paper', or 'Scissors'");
         } 
-    return answer;
+    if (answer == "Rock"){
+        return po1;
+    } else if (answer == "Paper") {
+        return po2;
+    }  else {
+        return po3;
+     }
     } 
     
     
 const playerChoice = getPlayerChoice();
 console.log("Player Choice: " + playerChoice);
+console.log("Computer Choice: " + computerChoice);
 
 
+const result1 = "This one is a Draw!"
+const result2 = "You lose..."
+const result3 = "You win!"
+
+function playRound (computerChoice, playerChoice){
+   if (computerChoice == co1 && playerChoice == po1) {
+        return result1
+    } else if (computerChoice == co1 && playerChoice == po2) {
+        return result3
+    } else if (computerChoice == co1 && playerChoice == po3) {
+        return result2
+    }   else if (computerChoice == co2 && playerChoice == po1) {
+        return result2
+    }   else if (computerChoice == co2 && playerChoice == po2) {
+        return result1
+    }   else if (computerChoice == co2 && playerChoice == po3) {
+        return result3
+    }   else if (computerChoice == co3 && playerChoice == po1) {
+        return result3
+    }    else if (computerChoice == co3 && playerChoice == po2) {
+        return result2
+    }   else if (computerChoice == co3 && playerChoice == po3) {
+        return result1
+    }
+}
+
+let result = playRound(computerChoice, playerChoice);
+console.log(result);
+
+
+
+/*
+let playerScore = 0;
+let computerScore = 0;
+function changeScore*/
